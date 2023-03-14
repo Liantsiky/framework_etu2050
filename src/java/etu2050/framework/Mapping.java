cb8c7136598f72d9dbde0a5163f6fb7f9ab377dc
Liantsiky Rasoanaivo <liantsiky@pop-os.localdomain>
2023-03-14 14:27:00 +0300

    "Envoie du sprint 2"

---
+++ src/java/etu2050/framework/Mapping.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package etu2050.framework;
+
+/**
+ *
+ * @author liantsiky
+ */
+public class Mapping {
+    String className;
+    String method;
+}
